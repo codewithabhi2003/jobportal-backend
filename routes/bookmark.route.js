3bb6853ed385313152c2ec34c4e977382e12ce4f
codewithabhi2003 <abhishekvishwakarma1149@gmail.com>
2026-01-08 23:22:43 +0530

    routes

--- routes/bookmark.route.js
+++ routes/bookmark.route.js
@@ -1,10 +1,16 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { toggleBookmark, getSavedJobs } from "../controllers/bookmark.controller.js";
+import {
+  toggleBookmark,
+  getSavedJobs,
+} from "../controllers/bookmark.controller.js";
 
 const router = express.Router();
 
+/* SAVE / UNSAVE JOB */
 router.post("/:jobId", isAuthenticated, toggleBookmark);
-router.get("/get", isAuthenticated, getBookmarks);
+
+/* GET SAVED JOBS */
+router.get("/get", isAuthenticated, getSavedJobs);
 
 export default router;
