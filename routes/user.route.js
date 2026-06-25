969537213dd3def2f66b74c85d6ec006e274fd36
Abhishek Vishwakarma <156510068+codewithabhi2003@users.noreply.github.com>
2026-06-25 14:25:05 +0000

    feat: Make recruiter profile publicly accessible (remove auth requirement)

--- routes/user.route.js
+++ routes/user.route.js
@@ -9,6 +9,6 @@ router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
-router.route("/profile/:id").get(isAuthenticated, getUserById);
+router.route("/profile/:id").get(getUserById);
 
 export default router;
